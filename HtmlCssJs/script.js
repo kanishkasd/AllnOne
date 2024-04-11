48c4223f5b1cf42679e58c02adbbe251f58e76e1
kanishkasd <kanishkasandaru2048@gmail.com>
2024-04-11 16:05:46 +0530

    add some code to js file

---
+++ HtmlCssJs/script.js
@@ -0,0 +1,100 @@
+const cartBtn = document.getElementById("cart-btn");
+const cartContainer = document.getElementById("cart-container");
+const showHideCart = document.getElementById("show-hide-cart");
+const clearCart = document.getElementById("clear-cart");
+const totalItems = document.getElementById("total-items");
+const subTotal = document.getElementById("sub-total");
+const total = document.getElementById("total");
+const cardContainer = document.getElementById("dessert-card-container");
+const productContainer = document.getElementById("products-container");
+const isCartShowing = false;
+
+const products = [
+  {
+    id: 1,
+    name: "Vanilla Cupcakes (6 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+  {
+    id: 2,
+    name: "French Macaron",
+    price: 3.99,
+    category: "Macaron",
+  },
+  {
+    id: 3,
+    name: "Pumpkin Cupcake",
+    price: 3.99,
+    category: "Cupcake",
+  },
+  {
+    id: 4,
+    name: "Chocolate Cupcake",
+    price: 5.99,
+    category: "Cupcake",
+  },
+  {
+    id: 5,
+    name: "Chocolate Pretzels (4 Pack)",
+    price: 10.99,
+    category: "Pretzel",
+  },
+  {
+    id: 6,
+    name: "Strawberry Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 7,
+    name: "Chocolate Macarons (4 Pack)",
+    price: 9.99,
+    category: "Macaron",
+  },
+  {
+    id: 8,
+    name: "Strawberry Pretzel",
+    price: 4.99,
+    category: "Pretzel",
+  },
+  {
+    id: 9,
+    name: "Butter Pecan Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 10,
+    name: "Rocky Road Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 11,
+    name: "Vanilla Macarons (5 Pack)",
+    price: 11.99,
+    category: "Macaron",
+  },
+  {
+    id: 12,
+    name: "Lemon Cupcakes (4 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+];
+
+products.forEach(({ name, id, price, category }) => {
+  cardContainer.innerHTML = `
+        <div class="dessert-card">
+            <h2>${name}</h2>
+            <p class="dessert-price">$${price}</p>
+            <p class="product-category">Category: ${category}</p>
+
+            <button 
+            id="${id}"
+            class="btn add-to-cart-btn">Add to cart</button>
+
+        </div>
+        `;
+});
