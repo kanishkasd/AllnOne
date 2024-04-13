f7b6c2bac5a2dc1731a1dffb38f67383b8ddc5fa
kanishkasd <kanishkasandaru2048@gmail.com>
2024-04-13 10:12:27 +0530

    completed one

--- HtmlCssJs/script.js
+++ HtmlCssJs/script.js
@@ -1,14 +1,14 @@
-const cartBtn = document.getElementById("cart-btn");
 const cartContainer = document.getElementById("cart-container");
-const showHideCart = document.getElementById("show-hide-cart");
-const clearCart = document.getElementById("clear-cart");
-const totalItems = document.getElementById("total-items");
-const subTotal = document.getElementById("sub-total");
-const taxes = document.getElementById("taxes");
-const total = document.getElementById("total");
-const cardContainer = document.getElementById("dessert-card-container");
-const productContainer = document.getElementById("products-container");
-const isCartShowing = false;
+const productsContainer = document.getElementById("products-container");
+const dessertCards = document.getElementById("dessert-card-container");
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;
 
 const products = [
   {
@@ -86,27 +86,27 @@ const products = [
 ];
 
 products.forEach(({ name, id, price, category }) => {
-  cardContainer.innerHTML += `
-        <div class="dessert-card">
-            <h2>${name}</h2>
-            <p class="dessert-price">$${price}</p>
-            <p class="product-category">Category: ${category}</p>
-
-            <button 
-            id="${id}"
-            class="btn add-to-cart-btn">Add to cart</button>
-        </div>
-        `;
+  dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
+      </div>
+    `;
 });
 
 class ShoppingCart {
   constructor() {
     this.items = [];
     this.total = 0;
-    this.taxRate = 10.5;
+    this.taxRate = 8.25;
   }
 
-  addItem(id, product) {
+  addItem(id, products) {
     const product = products.find((item) => item.id === id);
     const { name, price } = product;
     this.items.push(product);
@@ -118,18 +118,20 @@ class ShoppingCart {
     });
 
     const currentProductCount = totalCountPerProduct[product.id];
-
     const currentProductCountSpan = document.getElementById(
       `product-count-for-id${id}`
     );
 
     currentProductCount > 1
       ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
-      : (productContainer.innerHTML += `<div id="dessert${id}" class="product">
-        <p><span class="product-count" id="product-count-for-id${id}></span>  
+      : (productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+        <p>
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
         </p>
         <p>${price}</p>
-      </div>`);
+      </div>
+      `);
   }
 
   getCounts() {
@@ -138,30 +140,53 @@ class ShoppingCart {
 
   clearCart() {
     if (!this.items.length) {
-      alert("Your cart is already empty!!!");
+      alert("Your shopping cart is already empty");
       return;
     }
 
     const isCartCleared = confirm(
-      "Are you sure you want to clear all your cart items?"
+      "Are you sure you want to clear all items from your shopping cart?"
     );
 
     if (isCartCleared) {
       this.items = [];
       this.total = 0;
-      productContainer.innerHTML = "";
-      totalItems.textContent = 0;
-      subTotal.textContent = 0;
-      taxes.textContent = 0;
-      total.textContent = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
     }
   }
 
   calculateTaxes(amount) {
-    return parseFloat(((this.taxRate * amount) / 100).toFixed(2));
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
 
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
   }
 }
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    cart.addItem(Number(event.target.id), products);
+    totalNumberOfItems.textContent = cart.getCounts();
+    cart.calculateTotal();
+  });
+});
+
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+  showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+  cartContainer.style.display = isCartShowing ? "block" : "none";
+});
