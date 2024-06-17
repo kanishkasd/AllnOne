0e4d4b178c0a9bf332924f718499cb493429767e
kanishkasd <kanishkasandaru2048@gmail.com>
2024-06-17 19:34:10 +0530

    delete button component

--- todolistNext/src/app/page.tsx
+++ todolistNext/src/app/page.tsx
@@ -1,10 +1,13 @@
 import TaskAdd from "@/components/AddTasks/TaskAdd";
 import styles from "./page.module.css";
 export default function Home() {
+  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Just do it.|</h1>
-      <TaskAdd />
+      <TaskAdd handleClick={handleClick} />
     </div>
   );
 }
