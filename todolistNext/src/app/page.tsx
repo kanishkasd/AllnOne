8d398621d4a5e379984ad711e21543cdfa7293d7
kanishkasd <kanishkasandaru2048@gmail.com>
2024-06-17 21:18:22 +0530

    deleted onclick event

--- todolistNext/src/app/page.tsx
+++ todolistNext/src/app/page.tsx
@@ -1,13 +1,10 @@
 import TaskAdd from "@/components/AddTasks/TaskAdd";
 import styles from "./page.module.css";
 export default function Home() {
-  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Just do it.|</h1>
-      <TaskAdd handleClick={handleClick} />
+      <TaskAdd />
     </div>
   );
 }
