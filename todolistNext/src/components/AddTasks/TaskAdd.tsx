b63d23e8af07d1c723f7eeae136677c1ce4d7c08
kanishkasd <kanishkasandaru2048@gmail.com>
2024-07-12 21:02:09 +0530

    made changes after long time

--- todolistNext/src/components/AddTasks/TaskAdd.tsx
+++ todolistNext/src/components/AddTasks/TaskAdd.tsx
@@ -1,29 +1,12 @@
-import React, { useState } from "react";
-import styles from "./page.module.css";
-import { Roboto } from "next/font/google";
+"use client";
+import React from "react";
 
 const TaskAdd = () => {
-  const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
-  const handleClick = () => {
-    if (task.trim() !== "") {
-      setTasks([...tasks]);
-    }
-  };
   return (
-    <div className={styles.container}>
-      <form className={styles.form}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Type your task here...."
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <button onClick={(e) => handleClick} className={styles.button}>
-          ADD
-        </button>
-      </form>
-    </div>
+    <form>
+      <label htmlFor="addTasks">Add your tasks here </label>
+      <input type="text" id="addTasks" placeholder="Add your task" />
+    </form>
   );
 };
 
